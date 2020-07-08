7b0d9a38e82bb7d923c9b50007032b7faf26efdc
Célio Mendes <celio.mendes@soundparticles.com>
2020-07-08 18:07:16 +0100

    Added missing route

--- rest_api/simple_supply_rest_api/main.py
+++ rest_api/simple_supply_rest_api/main.py
@@ -104,6 +104,7 @@ def start_rest_api(host, port, messenger, database):
     app.router.add_patch('/voters/{voterId}/type', handler.update_voter_type)
     app.router.add_get('/voters/admins', handler.list_admins)
     app.router.add_get('/voters/{voterId}', handler.get_voter)
+    app.router.add_get('/voters/admins/{voterId}/elections', handler.list_admin_elections)
 
     app.router.add_get('/votes/{voteId}', handler.list_vote)
     app.router.add_get('/votes/{voterId}/voter', handler.list_votes)
